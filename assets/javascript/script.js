e1b6744e903097147e0bf2a542cbda339df7796c
adam fader <adamfader@gmail.com>
2016-06-19 15:48:11 -0400

    added login signup modals and authentication for firebase

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,18 @@
 $(document).on('ready', function(){
 
+    // Firebase link
+    var dataRef = new Firebase("https://i-was-here.firebaseio.com/");
+    // Initial Values
+    var authData = dataRef.getAuth();
+    
+    if(authData !== null){ //checks to see if client is authenticated
+
+        //hide login-button and show logout-button if client is authenticated
+        $("#login-button").addClass('hide').removeClass('show');
+        $("#logout-button").addClass('show').removeClass('hide');
+
+    }
+
 	$('#search').on('focus', function(){
 		$('.search-cover').addClass('no-height');
 	});
@@ -16,37 +29,97 @@ $(document).on('ready', function(){
 		//grabs the value from the input textfield
 		var term = $('#search').val().trim(); 
 
-    //query string for api that includes search parameter
-    var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+ term +"&key=AIzaSyC-OI8taHVJIYUQuUFM2zqo3gigV0O5QiU";
+        //query string for api that includes search parameter
+        var queryURL = "https://crossorigin.me/https://maps.googleapis.com/maps/api/place/textsearch/json?query="+ term +"&key=AIzaSyC-OI8taHVJIYUQuUFM2zqo3gigV0O5QiU";
 
-    //ajax makes request and returns the response
-    $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+        //ajax makes request and returns the response
+        $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
 
-         var results1 = response.data;
+        	console.log(response.results);
 
-         for(var i=0; i < results1.length; i++){
+             var locationData = response.results;
 
-             var newDiv = $('<div class="item">')//creates a div
+             for(var i=0; i < locationData.length; i++){
 
-             // var rating = results[i].rating;//grabs the rating from response
+                 var newDiv = $('<div class="item">')//creates a div
 
-             //var rate = $('<p>').text("Rating: " + rating);//create a <p> with a textnode of the rating
+                 // var rating = results[i].rating;//grabs the rating from response
 
-             var testRef = $('<p>');//creates a new image element
-             testRef.text(results1[i].results.reference);//added still src attribute
-             //newsImage.text('data-animate', results[i].images.fixed_height.url);//stores animated img url in data-
-             //newsImage.attr('data-still', results[i].images.fixed_height_still.url);//stores still img url in data-
-             //newsImage.attr('data-state', 'still');//added data attribute
-             //newsImage.addClass('image');//added class to the image
+                 //var rate = $('<p>').text("Rating: " + rating);//create a <p> with a textnode of the rating
 
-             //newDiv.append(rate)//appends the rating to the div
-             newDiv.append(testRef)//appends the image to the div
+                 var testRef = $('<img>');//creates a new image element
+                 testRef.attr('src', locationData[i].photos);
+                 //testRef.text(locationData[i].photos);//added still src attribute
+                 //newsImage.text('data-animate', results[i].images.fixed_height.url);//stores animated img url in data-
+                 //newsImage.attr('data-still', results[i].images.fixed_height_still.url);//stores still img url in data-
+                 //newsImage.attr('data-state', 'still');//added data attribute
+                 //newsImage.addClass('image');//added class to the image
 
-             $('#main-content').prepend(newDiv);//prepends entire image div to image-area div
+                 //newDiv.append(rate)//appends the rating to the div
+                 newDiv.append(testRef)//appends the image to the div
 
-         }
-        
-    }); 
-});
+                 $('#main-content').prepend(newDiv);//prepends entire image div to image-area div
+
+             }
+            
+        }); 
+    });
+
+    //Listens for Login Submit Button Click
+  $("#loginSubmit").on("click", function() {
+
+    //retrieve values from input fields and trims leading white space
+    var loginEmail = $('#loginEmail').val().trim();
+    var loginPass = $('#loginPass').val().trim();
+
+    //Checks Firebase users against submitted login credentials
+      dataRef.authWithPassword({
+      email    : loginEmail,
+      password : loginPass
+            }, function(error, authData) {
+              if (error) {
+                console.log("Login Failed!", error);
+              } else {
+                console.log("Authenticated successfully with payload:", authData);
+                remember: "sessionOnly" //User is only logged in for the life of the page
+                //Hide the Login Panel, Login Button and show Logout Button after successful login
+                $("#logout-button, #login-button").toggleClass('hide show');
+              }
+        });
+      // Don't refresh the page!
+    return false;
+  });
+
+  //Listens for SignUp Submit Button Click
+  $("#signSubmit").on("click", function() {
+
+    //retrieve values from input fields and trims leading white space
+    var signEmail = $('#signEmail').val().trim();
+    var signPass = $('#signPass').val().trim();
+
+    //Creates a new user in Firebase with submitted credentials
+      dataRef.createUser({
+      email    : signEmail,
+      password : signPass
+            }, function(error, userData) {
+              if (error) {
+                console.log("Error creating user:", error);
+              } else {
+                console.log("Successfully created user account with uid:", userData.uid);
+              }
+            });
+      // Don't refresh the page!
+    return false;
+  });
+
+  //Listens for Logout Button Click
+  $("#logout-button").on("click", function() {
+
+    // Unauthenticate the client
+        dataRef.unauth();
+        // Hide Logout button and show Login button
+        $("#logout-button, #login-button").toggleClass('hide show');
+
+  });
 
 });
