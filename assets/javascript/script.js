0316a059023457178d3b7152001a772038a6332f
adam fader <adamfader@gmail.com>
2016-06-20 19:32:55 -0400

    checking to make sure I'm on up to date code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -49,12 +49,14 @@ $(document).on('ready', function(){
             // Get a database reference to user node in DB
             var ref = new Firebase("https://i-was-here.firebaseio.com/users/" + user);
 
-            ref.on("child_added", function(snapshot){
-
-                console.log(snapshot);
-                console.log(snapshot.key());
-
-            })
+            // Attach an asynchronous callback to read the data from user node in DB
+            ref.on("value", function(snapshot) {
+              // console.log(snapshot.val());
+              // console.log(user);
+              // console.log(snapshot.val().username);
+            }, function (errorObject) {
+              console.log("The read failed: " + errorObject.code);
+            });
         }else{
             //if user is not logged in redirect to home page
             window.location.replace("index.html");  
