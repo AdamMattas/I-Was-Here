724dc51caf69aa3b1d069642e9f27679737e34f6
David Li <jdavidli01@outlook.com>
2016-06-20 21:11:55 -0400

    js changes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,38 +14,63 @@ $(document).on('ready', function(){
 		// $("#intro-image").addClass('hide');
 
 		//grabs the value from the input textfield
-		var term = $('#search').val().trim(); 
+		var city = $('#search').val().trim(); 
 
     //query string for api that includes search parameter
-    var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+ term +"&key=AIzaSyC-OI8taHVJIYUQuUFM2zqo3gigV0O5QiU";
+    var key = "cbd1ecb89687e74e";
+    var stateAbbreviation = "NY";
+    var queryURL = "http://api.wunderground.com/api/" + key +/*Your_Key*/"/conditions/q/" + stateAbbreviation + "/" + city + ".json";
+    
 
     //ajax makes request and returns the response
-    $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+    $.ajax({url: queryURL, method: 'GET'})
 
-         var results1 = response.data;
+        .done(function(response) {
 
-         for(var i=0; i < results1.length; i++){
+        console.log(response);
 
-             var newDiv = $('<div class="item">')//creates a div
+        var results1 = response.current_observation.forecast_url;
 
-             // var rating = results[i].rating;//grabs the rating from response
+        console.log(results1);
+
+        //var web = "http://www.wunderground.com/US/NY/Brooklyn.html";
+
+        var weatherDiv = $('<div>');
+
+        var frame = $('<iframe>');
+
+        frame.attr('src', results1);
+        frame.width(500);
+        frame.height(500);
+
+        weatherDiv.append(frame);
+
+        $('#main-content').prepend(weatherDiv);
+
+
+
+        //$('.weather').load(web);
+
+
+        //var newDiv = $('<div class="item">')//creates a div
+
+        //var forcast = results1.forecast_url;//grabs the rating from response
+        //var newFrame = $('<iframe class="forecast">')
+
 
              //var rate = $('<p>').text("Rating: " + rating);//create a <p> with a textnode of the rating
 
-             var testRef = $('<p>');//creates a new image element
-             testRef.text(results1[i].results.reference);//added still src attribute
+             //var testRef = $('<p>');//creates a new image element
+             //testRef.text(results1[i].results.reference);//added still src attribute
              //newsImage.text('data-animate', results[i].images.fixed_height.url);//stores animated img url in data-
              //newsImage.attr('data-still', results[i].images.fixed_height_still.url);//stores still img url in data-
              //newsImage.attr('data-state', 'still');//added data attribute
              //newsImage.addClass('image');//added class to the image
 
              //newDiv.append(rate)//appends the rating to the div
-             newDiv.append(testRef)//appends the image to the div
+             //newDiv.append(testRef)//appends the image to the div
 
-             $('#main-content').prepend(newDiv);//prepends entire image div to image-area div
-
-         }
-        
+             //$('#main-content').prepend(newDiv);//prepends entire image div to image-area div    
     }); 
 });
 
